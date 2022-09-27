b264a322af8cbab943c42d74917e23622159c60b
Alexandre Dulaunoy <a@foo.be>
2022-09-27 09:01:05 +0200

    fix: [build] fixed

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle exec jekyll build
-rsync -v -4 -rz --checksum _site adulau@kb.quuxlabs.com:/home/adulau/website/misp-standard.org/
+rsync -av -4 -rz --checksum _site/ adulau@kb.quuxlabs.com:/home/adulau/website/misp-standard.org/
