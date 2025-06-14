0edd29d27e7bd5a0b91cc30a3e946c6ee1138e6b
Alexandre Dulaunoy <a@foo.be>
2025-06-14 14:33:30 +0200

    chg: [build] updated server

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle exec jekyll build
-rsync -av -4 -rz --checksum _site/ adulau@kb.quuxlabs.com:/home/adulau/website/misp-standard.org/
+rsync -av -4 -rz --checksum _site/ adulau@www.misp-standard.org:/home/adulau/website/misp-standard.org/
